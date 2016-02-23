27429ed589a86667ef75b62109f7ed4276346253
Sui.N.Zhiyuan_main_pc <sui.nerd.zhiyuan@gmail.com>
2016-02-23 11:14:15 +0800

    modify sudoer

--- run_by_root.py
+++ run_by_root.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-print("hello world!")
+import os,sys,stat
+
+print("Start...")
+
+def conf_sudo():
+	print("Config sudo for admin...")
+	filename = "/etc/sudoers"
+	os.chmod(filename,stat.S_IRUSR|stat.S_IWUSR|stat.S_IRGRP)
+	file = open(filename,"a")
+	file.write("#no password for admin user\n")
+	file.write("dmin ALL=(ALL) NOPASSWD:ALL\n")
+	file.close()
+
+conf_sudo();
+
